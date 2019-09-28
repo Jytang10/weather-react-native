d78c6a2b0dee6f8ef78e559f904279e8c549aad8
James Tang <jytang10@gmail.com>
2019-09-28 16:55:16 -0700

    Added error handling display

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import Weather from "./Weather";
 
 export default class App extends React.Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null,
   };
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(
@@ -14,13 +15,15 @@ export default class App extends React.Component {
         });
       },
       error => {
-        console.log(error);
+        this.setState({
+        error: error
+        })
       }
     );
   }
 
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden="true"></StatusBar>
@@ -29,6 +32,7 @@ export default class App extends React.Component {
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the weather...</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -40,6 +44,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
   loading: {
     flex: 1,
